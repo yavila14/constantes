1e37c53c18362f1a843462d3d7143b549a7bd84e
Yeimy <yei14@hotmail.com>
2020-12-09 18:48:30 -0500

    Se agrega gitignore

---
+++ mainconstante.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	const int segundo=60;
+	const long diasa=365;
+	const float e=2.71828;
+	const double PI=3.14159;
+	const bool t=0;
+	const char dias='l';
+	
+	std::cout << "Mostrando el valor de segundos " <<segundo <<"\n";
+	std::cout << "Mostrando el numero de dias del ano " <<diasa <<"\n";
+	std::cout << "Mostrando el numero e " <<e <<"\n";
+	std::cout << "Mostrando el numero PI " <<PI <<"\n";
+	std::cout << "Mostrando el valor true " <<t <<"\n";
+	std::cout << "Mostrando dia de la semana " <<dias <<"\n";
+	return 0;
+}
